6d487e14fec142a0ead600354ecb209a521522fe
irfanizudin <irfanizzudyn@gmail.com>
2022-09-29 14:01:01 +0700

    Initial Commit

---
+++ pokedex/pokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexApp.swift
+//  pokedex
+//
+//  Created by Irfan Izudin on 29/09/22.
+//
+
+import SwiftUI
+
+@main
+struct pokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
